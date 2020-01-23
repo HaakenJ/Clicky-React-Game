97bc6ca3e5a7b4e8cd2aa0371e4851558e94a8d4
Haakasaurus <kramerhjohnson@gmail.com>
2020-01-22 18:59:19 -0800

    Changed react-scripts to 3.2.0

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
 
-function Head(props) {
+function Head() {
   return (
     <header>
       <h1>Clicky Game!</h1>
       <h2>
-        Click on an image to earn points, but don't click on any more than once!
+        Click on an image to earn points, but don&apost click on any more than once!
       </h2>
     </header>
   )
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -3,15 +3,20 @@ import React from 'react';
 import './style.css';
 
 // eslint-disable-next-line require-jsdoc
-function Image(props) {
+function Image({
+  id,
+  shake,
+  image,
+  onClick
+}) {
   return (
     <div
       role='img'
-      data-id={props.id}
+      data-id={id}
       aria-label='click item'
-      className={props.shake ? 'click-item shake' : 'click-item'}
-      style={{backgroundImage: `url(${props.image})`}}
-      onClick={props.onClick}
+      className={shake ? 'click-item shake' : 'click-item'}
+      style={{backgroundImage: `url(${image})`}}
+      onClick={onClick}
     >
     </div>
   );
