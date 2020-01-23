84a455a42cf9bd6f173ab731f43c997eeb2914a5
Haakasaurus <kramerhjohnson@gmail.com>
2020-01-22 16:37:27 -0800

    Changed core.ignorecase to false.  Should update Image directory name.

---
+++ src/components/Image/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './style.css';
+
+// eslint-disable-next-line require-jsdoc
+function Image(props) {
+  return (
+    <div
+      role='img'
+      data-id={props.id}
+      aria-label='click item'
+      className={props.shake ? 'click-item shake' : 'click-item'}
+      style={{backgroundImage: `url(${props.image})`}}
+      onClick={props.onClick}
+    >
+    </div>
+  );
+}
+
+export default Image;
